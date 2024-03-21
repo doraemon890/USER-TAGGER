38efa1fbcfd8bd6bad07fe596c894d742c04cc91
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-22 01:31:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,77 +46,69 @@ async def help(event):
         )
     )
 
+  
 @client.on(events.NewMessage(pattern="^/utag ?(.*)"))
 async def mentionall(event):
-    chat_id = event.chat_id
-    if event.is_private:
-        return await event.respond("__This command can be used in groups and channels!__")
-    
+  chat_id = event.chat_id
+  if event.is_private:
+    return await event.respond("__This command can be use in groups and channels!__")
+  
+  is_admin = False
+  try:
+    partici_ = await client(GetParticipantRequest(
+      event.chat_id,
+      event.sender_id
+    ))
+  except UserNotParticipantError:
     is_admin = False
-    try:
-        partici_ = await client(GetParticipantRequest(
-            event.chat_id,
-            event.sender_id
-        ))
-    except UserNotParticipantError:
-        is_admin = False
-    else:
-        if (
-            isinstance(
-                partici_.participant,
-                (
-                    ChannelParticipantAdmin,
-                    ChannelParticipantCreator
-                )
-            )
-        ):
-            is_admin = True
-    if not is_admin:
-        return await event.respond("__Only admins can mention all members!__")
-    
-    if event.pattern_match.group(1) and event.is_reply:
-        return await event.respond("__Give me one argument!__")
-    elif event.pattern_match.group(1):
-        mode = "text_on_cmd"
-        msg = event.pattern_match.group(1)
-    elif event.is_reply:
-        mode = "text_on_reply"
-        replied_msg = await event.get_reply_message()
-        if replied_msg == None:
-            return await event.respond("__I can't mention members for older messages! (messages which are sent before I'm added to this group)__")
-        # Extract mentions from entities
-        mentioned_usernames = []
-        for entity in replied_msg.entities:
-            if isinstance(entity, MessageEntityMention):
-                mentioned_usernames.append(entity.plain_text)
-        # Tag the users in the replied message
-        tagged_users = ' '.join([f"@{username}" for username in mentioned_usernames])
-        await event.respond(tagged_users)
-        msg = replied_msg
-    else:
-        return await event.respond("__Reply to a message or give me some text to mention others!__")
-    
-    spam_chats.append(chat_id)
-    usrnum = 0
-    usrtxt = ''
-    async for usr in client.iter_participants(chat_id):
-        if not chat_id in spam_chats:
-            break
-        usrnum += 1
-        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-        if usrnum == 5:
-            if mode == "text_on_cmd":
-                txt = f"{usrtxt}\n\n{msg}"
-                await client.send_message(chat_id, txt)
-            elif mode == "text_on_reply":
-                await msg.reply(usrtxt)
-            await asyncio.sleep(2)
-            usrnum = 0
-            usrtxt = ''
-    try:
-        spam_chats.remove(chat_id)
-    except:
-        pass
+  else:
+    if (
+      isinstance(
+        partici_.participant,
+        (
+          ChannelParticipantAdmin,
+          ChannelParticipantCreator
+        )
+      )
+    ):
+      is_admin = True
+  if not is_admin:
+    return await event.respond("__Only admins can mention all members!__")
+  
+  if event.pattern_match.group(1) and event.is_reply:
+    return await event.respond("__Give me one argument!__")
+  elif event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.is_reply:
+    mode = "text_on_reply"
+    msg = await event.get_reply_message()
+    if msg == None:
+        return await event.respond("__I can't mention members for older messages! (messages which are sent before I'm added to this group)__")
+  else:
+    return await event.respond("__Reply to a message or give me some text to mention others!__")
+  
+  spam_chats.append(chat_id)
+  usrnum = 0
+  usrtxt = ''
+  async for usr in client.iter_participants(chat_id):
+    if not chat_id in spam_chats:
+      break
+    usrnum += 1
+    usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+    if usrnum == 5:
+      if mode == "text_on_cmd":
+        txt = f"{usrtxt}\n\n{msg}"
+        await client.send_message(chat_id, txt)
+      elif mode == "text_on_reply":
+        await msg.reply(usrtxt)
+      await asyncio.sleep(2)
+      usrnum = 0
+      usrtxt = ''
+  try:
+    spam_chats.remove(chat_id)
+  except:
+    pass
 
 
 @client.on(events.NewMessage(pattern="^/cancel$"))
